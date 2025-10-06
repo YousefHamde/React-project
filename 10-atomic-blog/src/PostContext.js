608299c889e59117492b5f2488f565db5a72562c
YousefHamde <youssef416422@fsc.bu.edu.eg>
2025-10-06 21:46:17 +0300

    Add files via upload

---
+++ 10-atomic-blog/src/PostContext.js
@@ -0,0 +1,64 @@
+import { useState, createContext, useContext, useMemo } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+// Create A Context Provider
+const PostsContext = createContext();
+
+function PostProvider({children}){
+     const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  const value = useMemo( ()=> {
+    return {
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }
+  },[searchQuery , searchedPosts])
+
+    return (
+    // 2) PROVIDE VALUE TO CHILD COMPONENT
+    <PostsContext.Provider
+      value={value}
+    >{children}</PostsContext.Provider>
+    )
+}
+
+// custom Hook
+
+function usePost(){
+    const context = useContext(PostsContext);
+    if(context === undefined) throw new Error("PostContext was used outside of the PostProvider");
+    
+    return context;
+}
+
+export {PostProvider , usePost }
